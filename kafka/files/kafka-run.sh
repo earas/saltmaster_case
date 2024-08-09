95267c15e7ae88fe2303cf8ae601f5f6a9e3b92f
arase <ekrem.aras@hepsiburada.com>
2024-08-09 16:17:23 +0300

    kafka and zookeper v2

--- kafka/files/kafka-run.sh
+++ kafka/files/kafka-run.sh
@@ -4,5 +4,5 @@ export REGION=$(curl http://169.254.169.254/latest/dynamic/instance-identity/doc
 export NAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region $REGION --query "Tags[?Key=='Name'].Value" --output text)
 export CLUSTER_NAME=$(echo $NAME | cut -d '-' -f 2)
 export CLUSTER_NAME_COUNT=$(echo $NAME | cut -d '-' -f 4)
-echo  -e "zookeeper.connect="$(aws ec2 describe-instances --filters "Name=tag:Name,Values=Aras-$CLUSTER_NAME-zookeeper-*" --query 'Reservations[*].Instances[*].[PrivateIpAddress, Tags[?Key==`Name`].Value]' | jq -r '.[][] | "\(.[1][0]) \(.[0])"' | sort | grep -v null | awk '{print $2":2181"}'  | paste -sd, -) >> /opt/kafka_2.13-3.7.0/config/server.properties
+echo  -e "zookeeper.connect="$(aws ec2 describe-instances --filters "Name=tag:Name,Values=ARAS-$CLUSTER_NAME-zookeeper-*" --query 'Reservations[*].Instances[*].[PrivateIpAddress, Tags[?Key==`Name`].Value]' | jq -r '.[][] | "\(.[1][0]) \(.[0])"' | sort | grep -v null | awk '{print $2":2181"}'  | paste -sd, -) >> /opt/kafka_2.13-3.7.0/config/server.properties
 echo "broker.id=$CLUSTER_NAME_COUNT" >>  /opt/kafka_2.13-3.7.0/config/server.properties
